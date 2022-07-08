8fc087b110a5fc989e4df8e1a1764efdc958a519
Peter DeWitt <peter.dewitt@nrel.gov>
2022-07-08 10:41:08 -0600

    file rename

---
+++ R/pTITAN2-pkg.R
@@ -0,0 +1,9 @@
+#' Permutations of Treatment Labels and TITAN2 Analysis
+#'
+#' Permute treatment labels for taxa and environmental gradients to generate
+#' an empirical distribution of change points.
+#'
+#' @docType package
+#' @name pTITAN2
+NULL
+
